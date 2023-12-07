78c5dc92fd3cd4a03790e70aa4654b50181bb6bd
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-12-07 18:00:40 +0530

    moc-6 back deploy

---
+++ routes/blog.routes.js
@@ -0,0 +1,91 @@
+
+const express = require("express")
+const { blogModel } = require("../models/blog.model")
+const { auth } = require("../middleware/auth.middleware")
+const blogRouter = express.Router()
+
+
+
+blogRouter.post("/blogs", auth, async (req, res) => {
+
+    try {
+
+        const blog = new blogModel(req.body)
+         await blog.save()
+         res.status(200).send({ "msg":  blog})
+
+    } catch (error) {
+        res.status(400).send({ "error": err.message })
+    }
+
+})
+
+// get
+blogRouter.get("/blogs", auth, async (req, res) => {
+  
+//   const {asc} = req.query
+//   const query = {...req.query}
+
+//   delete query.asc
+
+//   if(asc){
+//     if(!query.date){
+//         query.date = {}
+//     }
+//     query.date.$lt = asc
+//   }
+
+   try {
+
+       const blog = await  blogModel.find()
+        
+        res.status(200).send({ "msg":  blog})
+
+   } catch (error) {
+       res.status(400).send({ "error": error.message })
+   }
+
+})
+
+
+
+
+
+// patch
+blogRouter.patch("/blogs/:id",auth, async (req, res) => {
+     const {id} = req.params
+    try {
+
+        const blog = await  blogModel.findByIdAndUpdate({_id : id}, req.body)
+         
+         res.status(200).send({ "msg":  blog})
+
+    } catch (error) {
+        res.status(400).send({ "error": error.message })
+    }
+
+})
+
+// delete
+blogRouter.delete("/blogs/:id", auth, async (req, res) => {
+     const {id} = req.params
+    try {
+
+        const blog = await  blogModel.findByIdAndDelete({_id : id}, req.body)
+         
+         res.status(200).send({ "msg":  blog})
+
+    } catch (error) {
+        res.status(400).send({ "error": error.message })
+    }
+
+})
+
+
+
+
+
+
+module.exports = {
+    blogRouter
+}
